a4dbc5c5691a9c1612fe8ac9e13033d9c1dad240
thundering redstar <wvongphanith30@gmail.com>
2022-09-29 03:42:02 +0000

    functionIZING :troll: + choosing a jimmy

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,9 +1,9 @@
 """
-Duo <tpng name here lol> Hui Wang, William Vongphanith
+Duo XeraRedStar: Hui Wang, William Vongphanith
 SoftDev
 K05 -- Bitstream
 2022-09-28
-Time Spent: <t> hours
+Time Spent: 0.6 hours
 """
 
 import random
@@ -17,61 +17,69 @@ def next_element(array, element):
 # read file
 f = open("krewes.txt", "r")
 
-# it's called bitstream so we are not going to split/split again
-krewes = {}
-
 # read file (one line) letter by letter
 krewes_stream = f.read()
-
-current_period = ""
-current_name = ""
-current_ducky = ""
-
-current_character = ""
-dollar_sign_count = 0
-at_sign_count = 0
-
-current_parsing_type = "period"
 
 # Input looks like this:
 # pd$$$name$$$ducky@@@anotherpd$$$anothername$$$anotherducky@@@...
 
-for i in krewes_stream:
-    if i == "$":
-        dollar_sign_count += 1
-        at_sign_count = 0
-        current_character = "$"
-    elif i == "@":
-        at_sign_count += 1
-        dollar_sign_count = 0
-        current_character = "@"
-    else:
-        current_character = i
-        if current_parsing_type == "period":
-            current_period += current_character
-        elif current_parsing_type == "name":
-            current_name += current_character
-        elif current_parsing_type == "ducky":
-            current_ducky += current_character
+def parse_stream(stream):
+    krewes = {}
 
-    if current_character == "$" and dollar_sign_count == 3:
-        dollar_sign_count = 0
-        current_parsing_type = next_element(parsing_types, current_parsing_type)
-
-    if current_character == "@" and at_sign_count == 3:
-        at_sign_count = 0
-        current_parsing_type = next_element(parsing_types, current_parsing_type)
-
-        krewe_member = (current_name, current_ducky)
-        # Create list of krewe members
-        if current_period in krewes:
-            krewes[current_period].append(krewe_member)
+    current_period = ""
+    current_name = ""
+    current_ducky = ""
+    
+    current_character = ""
+    dollar_sign_count = 0
+    at_sign_count = 0
+    
+    current_parsing_type = "period"
+    
+    for i in stream:
+        if i == "$":
+            dollar_sign_count += 1
+            at_sign_count = 0
+            current_character = "$"
+        elif i == "@":
+            at_sign_count += 1
+            dollar_sign_count = 0
+            current_character = "@"
         else:
-            krewes[current_period] = [krewe_member]
+            current_character = i
+            if current_parsing_type == "period":
+                current_period += current_character
+            elif current_parsing_type == "name":
+                current_name += current_character
+            elif current_parsing_type == "ducky":
+                current_ducky += current_character
+        
+        if current_character == "$" and dollar_sign_count == 3:
+            dollar_sign_count = 0
+            current_parsing_type = next_element(parsing_types, current_parsing_type)
+        
+        if current_character == "@" and at_sign_count == 3:
+            at_sign_count = 0
+            current_parsing_type = next_element(parsing_types, current_parsing_type)
+            
+            krewe_member = (current_name, current_ducky)
+            # Create list of krewe members
+            if current_period in krewes:
+                krewes[current_period].append(krewe_member)
+            else:
+                krewes[current_period] = [krewe_member]
+            
+            current_period = ""
+            current_name = ""
+            current_ducky = ""
 
-        current_period = ""
-        current_name = ""
-        current_ducky = ""
+    return krewes
 
-print(krewes)
+# Now choose a random krewe member
+def choose_krewe_member(krewes):
+    period = random.choice(list(krewes.keys()))
+    krewe_member = random.choice(krewes[period])
+    return (period, krewe_member)
 
+krewes = parse_stream(krewes_stream)
+print("Chose " + str(choose_krewe_member(krewes)[1][0]) + " from period " + str(choose_krewe_member(krewes)[0]) + " (their ducky is " + str(choose_krewe_member(krewes)[1][1]) + ")")
