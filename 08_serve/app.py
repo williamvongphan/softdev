0c1b3f9e9dd512b8208ff1b99cab32a2e4937d56
williamvongphan <wvongphanith30@gmail.com>
2022-10-06 09:23:57 -0400

    header, thanks elizabeth

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,6 +1,28 @@
-"""
-
-"""
+'''
+TEAM KrEWes: Elizabeth Paperno, Raven (Ruiwen) Tang, William Vongphanith
+SoftDev
+K08 -- Using Flask to Display Input on a Server
+2022-10-06
+Time Spent: 0.6 hours
+DISCO
+___________
+- You can import methods from a file using  from [file] import [function]
+- It would be good practice to have the function where generate output called seperate in a main function
+- host='0.0.0.0' allows you to listen on network and local host
+- Good practice to call run() in main() explicity.
+QCC
+___________
+- Do you not include the extension (e.g numpercruncher.py vs just numbercruncher)
+when you import from python files? If we ever import anything that is not a module
+or python file should we include the extension?
+- If we had multiple functions defined would all the functions be run and displayed on the web server?
+OPS SUMMARY
+___________
+- First we essentially copied the flask starter code. Then we imported the choose_occupation
+function from our origninal numbercruncher file in 06_py-csv. We then redefined the main function
+in flask to return the output of numbercruncher (instead of "No hablo queso!"). We then called
+app.run explicitly in main().
+'''
 
 from numbercruncher import choose_occupation
 from flask import Flask
