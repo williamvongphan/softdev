d16cc4d6fa4e8958d151bf2cc31531b2f64bbf29
williamvongphan <wvongphanith30@gmail.com>
2022-12-01 08:58:16 -0500

    uodate

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open('key_nasa.txt', 'r') as f:
+    with open('key_nasa.txt', 'r+') as f:
         key = f.read()
         response = requests.get('https://api.nasa.gov/planetary/earth/assets?lon=100.75&lat=1.5&date=2014-02-01&dim=0.15&api_key=' + key)
         data = response.json()
         return render_template('main.html', data=data['url'])
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port="5001")
